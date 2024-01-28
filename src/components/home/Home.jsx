11f0afc2e99c283532d5e0f91bb7eae5a75538ee
Zin Yaw Aung <77819958+zinyawaung@users.noreply.github.com>
2024-01-28 11:29:34 +0630

    changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -11,17 +11,9 @@ const Home =()=>{
                 <div className="home__container container grid">
                         <div className="home__content grid">
                                 <Social />
-
                                 <div className="home__img"></div>
-
                                 <Data />
-
                         </div>
-
-                        
-                       
-                        
-                        
                         <ScrollDown />
                 </div>
               </section>
